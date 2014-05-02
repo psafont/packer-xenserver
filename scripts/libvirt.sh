5f2999e40cd120ba0b623774644de5cfe1a6cb7c
David Scott <dave.scott@citrix.com>
2014-05-02 22:51:35 +0100

    Experimental definition for XenServer development nightly snapshot
    Signed-off-by: David Scott <dave.scott@citrix.com>

---
+++ scripts/libvirt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+
+#sudo yum --enablerepo=base,extra --nogpgcheck install libvirt-daemon libvirt-client -y
+
