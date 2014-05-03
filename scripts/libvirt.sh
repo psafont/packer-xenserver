dd5f03ff90fd93fa2f09fab592cf00f621982a1c
David Scott <dave.scott@citrix.com>
2014-05-03 16:23:47 +0100

    install libvirt-daemon-driver-xen
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- scripts/libvirt.sh
+++ scripts/libvirt.sh
@@ -4,5 +4,5 @@ wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
-#sudo yum --enablerepo=base,extra --nogpgcheck install libvirt-daemon libvirt-client -y
+sudo yum --enablerepo=base,extra --nogpgcheck install libvirt-daemon libvirt-client libvirt libvirt-daemon-driver-xen -y
 
