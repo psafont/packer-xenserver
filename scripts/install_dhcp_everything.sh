3a5100e6354a0da6ae86b9afb722da7f73bb31c2
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-03-31 17:13:46 +0100

    Install dhcp-everything as a firstboot.d script
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- scripts/install_dhcp_everything.sh
+++ scripts/install_dhcp_everything.sh
@@ -2,9 +2,8 @@
 
 mount /dev/sda1 /mnt
 
-cp /tmp/dhcp-everything /mnt/etc/init.d/dhcp-everything
-chmod 755 /mnt/etc/init.d/dhcp-everything
-ln -s ../init.d/dhcp-everything /mnt/etc/rc3.d/S99dhcp-everything
+cp /tmp/dhcp-everything /mnt/etc/firstboot.d/96-dhcp-everything
+chmod 755 /mnt/etc/firstboot.d/96-dhcp-everything
 
 # Clean up
 umount /mnt
