9675152402baabfa7d6b14b4683b93344a21c184
Edwin Török <edvin.torok@citrix.com>
2019-02-04 09:43:50 +0000

    Create box under xenserver namespace
    Otherwise it gets created under `edwintorok/` and attemps to upload to
    `xenserver/`, which won't work.
    
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- jenkins/create-vagrantcloud-box.sh
+++ jenkins/create-vagrantcloud-box.sh
@@ -4,5 +4,5 @@ set -x
 name=$1
 apikey=$2
 
-curl https://app.vagrantup.com/api/v1/boxes -X POST -d box[name]="$name" -d box[is_private]=false -d access_token=$apikey
+curl https://app.vagrantup.com/api/v1/boxes -X POST -d box[name]="$name" -d box[is_private]=false -d box[username]=xenserver -d access_token=$apikey
 
